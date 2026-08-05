b2cd62cdc5b33cc3ddc9b7dfc439580b7d861cf0
N Saquib <saquib@Ns-MacBook-Pro.local>
2026-08-05 16:14:29 +0530

    discount js changes

--- sharedLogic/src/commonMain/kotlin/com/cravrr/calculationengine/JsExports.kt
+++ sharedLogic/src/commonMain/kotlin/com/cravrr/calculationengine/JsExports.kt
@@ -2,7 +2,6 @@
 
 package com.cravrr.calculationengine
 
-import com.cravrr.calculationengine.applyOffer.OfferEngineImpl
 import com.cravrr.calculationengine.calculation.CalculationEngineImpl
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
@@ -34,56 +33,3 @@ fun refreshCalculationJs(
         isTaxIncluded
     )
 }
-
-@JsExport
-suspend fun applyOrderLevelOfferJs(
-    offerJson: String,
-    orderJson: String,
-    isTaxIncluded: Boolean,
-    onCompleted: (String, Boolean) -> Unit
-) {
-    val engine = OfferEngineImpl()
-    return engine.applyOrderLevelOffer(offerJson, orderJson, isTaxIncluded, onCompleted)
-}
-
-@JsExport
-suspend fun applyItemLevelOfferJs(
-    offerListJson: String,
-    orderJson: String,
-    isTaxIncluded: Boolean,
-    onCompleted: (String) -> Unit = {},
-) {
-    val engine = OfferEngineImpl()
-    return engine.applyItemLevelOffer(offerListJson, orderJson, isTaxIncluded, onCompleted)
-}
-
-@JsExport
-suspend fun applyCouponCodeOfferJs(
-    orderJson: String,
-    offerListJson: String,
-    isTaxIncluded: Boolean,
-    couponCode: String,
-    onCompleted: (String, Boolean) -> Unit
-): Boolean {
-    val engine = OfferEngineImpl()
-    return engine.applyCouponCodeOffer(
-        orderJson,
-        offerListJson,
-        isTaxIncluded,
-        couponCode,
-        onCompleted
-    )
-}
-
-@JsExport
-suspend fun isOfferValidOnCartJs(
-    orderJson: String,
-    offerJson: String
-): Boolean {
-    val engine = OfferEngineImpl()
-    return engine.isOfferValidOnCart(
-        offerJson,
-        orderJson
-    )
-}
-
